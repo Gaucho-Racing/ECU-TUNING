a634c587020a7b4a53e665ce7fab6f938291ba0f
Nikunj Parasar <nikunj.parasar@gmail.com>
2024-05-08 22:02:50 -0700

    sends something on CAN i guess

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import can
+import time
 
 def send_one():
     try:
         # Instantiate the bus with specific settings
-        with can.interface.Bus(interface='seeedstudio', channel='/dev/cu', bitrate=500000) as bus:
+    #    /dev/tty.usbmodem139128701 
+    #  /dev/tty.usbserial-2110
+        with can.interface.Bus(interface='seeedstudio', channel='/dev/tty.usbserial-2110', baud=1000000, frame_type='EXT', timeout=0.1, operation_mode='normal', bitrate=1000000) as bus:
             msg = can.Message(
-                arbitration_id=0xC0FFEE, data=[0, 25, 0, 1, 3, 1, 4, 1], is_extended_id=True
+                arbitration_id=0x6969, data=[0, 0, 0, 0, 0, 0, 0, 1], is_extended_id=True
             )
             bus.send(msg)
             print(f"Message sent on {bus.channel_info}")
@@ -15,4 +18,6 @@ def send_one():
         print("An unexpected error occurred:", e)
 
 if __name__ == "__main__":
-    send_one()
+    while True:
+        send_one()
+        time.sleep(1)
